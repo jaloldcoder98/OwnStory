1af29059be917dd73d8f5d6224b850f978543f5d
jaloldcoder98 <jaloldcoder@gmail.com>
2022-05-17 11:30:44 +0500

    Initial Commit

---
+++ OwnStory/OwnStoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  OwnStoryApp.swift
+//  OwnStory
+//
+//  Created by Jaloliddin on 5/13/22.
+//
+
+import SwiftUI
+
+@main
+struct OwnStoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
